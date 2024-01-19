5253403e0807d9c2de509f670c22aac21a3f91d5
mutkins <113161484+mutkins@users.noreply.github.com>
2024-01-19 14:30:06 +0500

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -3,9 +3,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.bot.api import TelegramAPIServer
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
-
+from config import PROXY
 storage = MemoryStorage()
 load_dotenv()
 
-bot = Bot(token=os.environ.get('tgBot_id'))
+bot = Bot(token=os.environ.get('tgBot_id'), proxy=PROXY)
 dp = Dispatcher(bot, storage=storage)
