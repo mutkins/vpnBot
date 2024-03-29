df86c989db660e61b655998b60b378341ddde08b
mutkins <113161484+mutkins@users.noreply.github.com>
2024-03-29 13:18:40 +0500

    Update db_init.py

--- db/db_init.py
+++ db/db_init.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # Declarative method
-engine = create_engine("sqlite:///db/ducky_vpn.db", echo=True)
+engine = create_engine("sqlite:////etc/ducky_vpn_data/ducky_vpn.db", echo=True)
 Base = declarative_base()
 
 
