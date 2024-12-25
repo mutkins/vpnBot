2fdaada0fe514f0d09187144e9fcbd3d32882e5a
Mikhail Utkin <mihut@rambler.ru>
2024-12-25 21:02:56 +0500

    fix

--- handlers/admin/actions.py
+++ handlers/admin/actions.py
@@ -26,7 +26,7 @@ async def add_key(message: types.Message):
     user = get_user_by_chat_id(chat_id=message.from_user.id)
     if not user:
         add_user(chat_id=message.from_user.id, username=message.from_user.username)
-    key_id = await add_new_key(name=message.text, chat_id=message.from_user.id, is_trial=True,
+    key_id = await add_new_key(name=message.get_args(), chat_id=message.from_user.id, is_trial=True,
                                server_name=TRIAL_SERVER_NAME, expired=None)
     activate_key(key_id=key_id)
     await send_active_keys_by_user(chat_id=message.from_user.id)
